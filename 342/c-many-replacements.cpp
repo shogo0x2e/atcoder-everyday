4cbb68b632c3dc9d8c5ba4019b3de3ceba3014da
Shogo Kitada <contact@shogo0x2e.com>
2024-02-28 00:59:06 +0900

    a-z の変換表を作って、最終的な文字列をその変換表に従って直せばいいじゃない、っていう発想がクリティカル

--- 342/c-many-replacements.cpp
+++ 342/c-many-replacements.cpp
@@ -8,4 +8,25 @@ int main() {
 
   std::string from, to;
 
+  // a-z に対して、最終的な変換表を作る
+  from = to = "abcdefghijklmnopqrstuvwxyz";
+
+  for (int i = 0; i < Q; i++) {
+    char c, d;
+    std::cin >> c >> d;
+    for (auto &&e: to) {
+      if (e == c) e = d;
+    }
+  }
+
+  for (auto &&e: S) {
+    for (int i = 0; i < 26; i++) {
+      if (e == from[i]) {
+        std::cout << to[i];
+      }
+    }
+  }
+
+  std::cout << std::endl;
+
 }
